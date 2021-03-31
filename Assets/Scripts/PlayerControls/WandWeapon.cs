acc4fd4868928d755444b4d3af363c3ea6e42732
MrMongoose <ben.ede@hotmail.co.uk>
2021-03-31 02:01:22 +0100

    Some Fixes from last push

--- Assets/Scripts/PlayerControls/WandWeapon.cs
+++ Assets/Scripts/PlayerControls/WandWeapon.cs
@@ -6,7 +6,7 @@ public class WandWeapon : MonoBehaviour
 {
     //This can be changed to a more accurate location (e.g. the location of the wand)
     //Might need to change the direction if this is done so as to go to the center of the screen
-    public GameObject target;
+    public GameObject projectileSpawn;
 
     //Projectile prefab must have the projectile script, a collider and a rigid body.
     public GameObject projectilePrefab;
@@ -39,16 +39,16 @@ public class WandWeapon : MonoBehaviour
     void FireProjectile()
     {
         //Pick a destination using the range of the weapon.
-        projectileDestination = target.transform.position + (target.transform.forward * range);
+        //projectileDestination = projectileSpawn.transform.position + (projectileSpawn.transform.forward * range);
 
         //Instantiate the projectile.
-        GameObject projectileObject = Instantiate(projectilePrefab, target.transform.position, Quaternion.identity);
+        GameObject projectileObject = Instantiate(projectilePrefab, projectileSpawn.transform.position, Quaternion.identity);
 
         //Set the damage of the projectile. Set the speed and direction too.
-        Projectile projecltileScipt = projectileObject.GetComponent<Projectile>();
-        projecltileScipt.SetDamage(weaponDamage);
-        projecltileScipt.SetRange(range);
+        Projectile projectileScript = projectileObject.GetComponent<Projectile>();
+        projectileScript.SetDamage(weaponDamage);
+        projectileScript.SetRange(range);
 
-        projectileObject.GetComponent<Rigidbody>().velocity = (projectileDestination - target.transform.position).normalized * projectileSpeed;
+        projectileObject.GetComponent<Rigidbody>().velocity = projectileSpawn.transform.forward * projectileSpeed;
     }
 }
